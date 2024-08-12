f0855dc9f714ff534f1dfd9c40d11c141521a6c2
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-08-11 22:08:38 -0700

    Create panorama.js

---
+++ panorama.js
@@ -0,0 +1,9 @@
+//this script created a siries of screenshots that can be used as a panorama for a resource pack
+
+ModAPI.require("player");
+
+ModAPI.addEventListener("sendchatmessage", (ev) => {
+  if (ev.message.toLowercase === ".pan") {
+    
+  }
+});
