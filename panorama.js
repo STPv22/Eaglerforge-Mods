423298a27f7f98b896d561ddc48cc82deb6a3e3c
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-09-13 14:02:18 -0700

    Delete panorama.js

--- panorama.js
+++
@@ -1,9 +0,0 @@
-//this script created a siries of screenshots that can be used as a panorama for a resource pack
-
-ModAPI.require("player");
-
-ModAPI.addEventListener("sendchatmessage", (ev) => {
-  if (ev.message.toLowercase === ".pan") {
-    
-  }
-});
