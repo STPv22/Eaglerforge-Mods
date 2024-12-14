bb82df66fba0d0aa1f5e3830f328efa232e23a2f
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-12-14 10:56:03 -0800

    Create dependency.js

---
+++ dependency.js
@@ -0,0 +1,9 @@
+ModAPI.meta.title("ModAPI dependency plugin");
+ModAPI.meta.description("https://www.youtube.com/watch?v=_RluM55dNOg");
+ModAPI.dependency ||= {};
+ModAPI.dependecy.register = function (object, string) {
+  
+};
+ModAPI.dependecy.require = function (modstring) {
+  
+};
