213e6723a10dd1029d4bdd9e2937f03d708d184c
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-12-14 20:47:23 -0800

    idk if I did it right.

--- dependency.js
+++ dependency.js
@@ -1,9 +1,23 @@
 ModAPI.meta.title("ModAPI dependency plugin");
-ModAPI.meta.description("https://www.youtube.com/watch?v=_RluM55dNOg");
-ModAPI.dependency ||= {};
-ModAPI.dependecy.register = function (object, string) {
-  
+ModAPI.meta.description('A little plugin that is not needed but could be usefull in the future.');
+ModAPI.dependecy ||= {};
+ModAPI.dependecy._registry ||= [];
+ModAPI.dependecy.register = function (modName, modObject) {
+    var mod = {id: modName}
+    if (typeof modObject === "object") {
+        modObj.mod = modObject;
+        return mod;
+    } else {
+        return;
+    }
 };
-ModAPI.dependecy.require = function (modstring) {
-  
+ModAPI.dependecy.require = function (modName) {
+    let modRequired;
+    try {
+        modRequired = ModAPI.dependecy._registry.find((a) => a = modstring);
+    } catch (err) {
+        alert(`Could not find mod ${modName}! Did you spell it correctly?`); //better message?
+        console.error(err);
+    }
+    return modRequired;
 };
